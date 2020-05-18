67e4d0c331cea2ed2757ba61036716cc73269e03
Zero_117 <aldoups116@gmail.com>
2020-05-18 12:21:55 -0500

    actualizacion 1.1

--- app/src/main/java/com/example/apppizzeria/MainActivity.java
+++ app/src/main/java/com/example/apppizzeria/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.apppizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
+    }
+
+    public void onClick(View v) {
+        Intent Psiguiente=null;
+        switch (v.getId()){
+            case R.id.ingresar:
+                Psiguiente = new Intent(MainActivity.this,menuActivity.class);
+                break;
+
+            case R.id.registrarse:
+                Psiguiente= new Intent(MainActivity.this,registroActivity.class);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/apppizzeria/menuActivity.java
@@ -0,0 +1,13 @@
+package com.example.apppizzeria;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
