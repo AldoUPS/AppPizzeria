52667c24a32885b628eabf498e2780f0de3ba82f
Zero_117 <aldoups116@gmail.com>
2020-05-19 19:14:28 -0500

    integracion de validaciones y otras interfaces

--- app/src/main/java/com/example/apppizzeria/MainActivity.java
+++ app/src/main/java/com/example/apppizzeria/MainActivity.java
@@ -7,14 +7,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity  {
     EditText et1, et2;
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
         et1 = (EditText) findViewById(R.id.correo);
         et2 = (EditText) findViewById(R.id.contrasena);
+
+        // codigo del boton de facebook
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.loginF);
+        // Callback
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent x = new Intent(MainActivity.this,menuActivity.class);
+                startActivity(x);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
     }
 
     public void ingresar (View v){
@@ -29,21 +62,21 @@ public class MainActivity extends AppCompatActivity  {
         startActivity(x);
     }
 
-    public boolean validar(){
+    public boolean validar() {
         boolean retorno = true;
 
         String T1 = et1.getText().toString();
         String T2 = et2.getText().toString();
 
-        if(T1.isEmpty()){
+        if (T1.isEmpty()) {
             et1.setError("este campo no puede quedar vacio");
             retorno = false;
         }
-        if(T2.isEmpty()){
+        if (T2.isEmpty()) {
             et2.setError("este campo no puede estar vacio");
             retorno = false;
         }
-        
+
         return retorno;
     }
 }
